a400cd322d9701421a2c361932183861bc42ae78
Bhuwan Pandit <bhuwanpandit109@gmail.com>
2024-09-22 13:55:20 +0100

    refact: add macro to print to vga text buffer

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,9 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello from Bhuwan Pandit!";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
+    println!("Hello World{}", "!");
 
     loop {}
 }
