4f446b7e79ef13fc91307cf6927cbaa77d042a97
Bhuwan Pandit <bhuwanpandit109@gmail.com>
2024-09-24 00:18:37 +0100

    refact: change to library crate for integration testing

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,11 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(ToyOperatingSys::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-mod printer;
-mod vga_buffer;
-mod exit_qemu;
-mod serial;
-
-use exit_qemu::{exit_qemu, QemuExitCode};
-
-
-/// panic handler for normal mode
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-
-/// panic handler for test mode
-#[cfg(test)]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
-
-#[cfg(test)]
-pub fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-    exit_qemu(QemuExitCode::Success);
-}
-
+use ToyOperatingSys::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -52,19 +17,16 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
-pub trait Testable {
-    fn run(&self) -> ();
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
 }
 
-impl<T: Fn()> Testable for T {
-    fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
-}
-
-#[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    ToyOperatingSys::test_panic_handler(info)
 }
